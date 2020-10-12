38b33cabbd8b9fe75774c2a6a370347655702905
ThanhTan <haitandongthap@gmail.com>
2020-10-12 23:17:07 +0900

    Example Clock with html css

---
+++ login2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>LOGIN FORM</h1>
+
+    <?php
+    $txt1 = "Learn PHP";
+    $txt2 = "W3Schools.com";
+    $x = 5;
+    $y = 4;
+
+    print "<h2>" . $txt1 . "</h2>";
+    print "Study PHP at " . $txt2 . "<br>";
+    print $x + $y;
+    ?>
+
+</body>
+
+</html>
